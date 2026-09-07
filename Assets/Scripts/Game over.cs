31c9475e8e629a62bc05ea7c284cb089276aa2d2
SurrealDude1 <pereiramacedoantonio007@gmail.com>
2026-09-07 02:02:49 +0100

    Animação
    Animação adicionada

--- Assets/Scripts/Game over.cs
+++ Assets/Scripts/Game over.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Interactions;
 
 public class GameOverOnTouch : MonoBehaviour
 {
     [SerializeField] private GameObject gameOverManager;
-
+[SerializeField] private Animator animator;
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         if (hit.collider.CompareTag("Game Over"))
         {
             gameOverManager.SetActive(true);
+            animator.SetTrigger("Idle");
         }
     }
 }
